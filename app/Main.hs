9418f6cf8e9d16b268263902e026227bd7bbe754
Nestor Morales <moralesnestoralejandro@gmail.com>
2024-06-02 20:28:23 -0500

    Ya sirve el extractor de datos

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
-import qualified Data.ByteString.Lazy as B
-import Data.Aeson (decode)
-import ETL (ShowInfo)
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Vector as V
+
+-- Definimos un tipo de dato para representar cada fila del CSV.
+data ShowInfo = ShowInfo
+  { showId :: !String
+  , title :: !String
+  , director :: !(Maybe String)
+  , cast :: !(Maybe String)
+  , country :: !String
+  , dateAdded :: !String
+  , releaseYear :: !Int
+  , rating :: !String
+  , duration :: !String
+  , listedIn :: !String
+  , description :: !String
+  , type' :: !String
+  } deriving (Show)
+
+-- Instancia para convertir cada fila del CSV a nuestro tipo de dato.
+instance FromNamedRecord ShowInfo where
+  parseNamedRecord r = ShowInfo <$> r .: "show_id"
+                                 <*> r .: "title"
+                                 <*> r .: "director"
+                                 <*> r .: "cast"
+                                 <*> r .: "country"
+                                 <*> r .: "date_added"
+                                 <*> r .: "release_year"
+                                 <*> r .: "rating"
+                                 <*> r .: "duration"
+                                 <*> r .: "listed_in"
+                                 <*> r .: "description"
+                                 <*> r .: "type"
 
 main :: IO ()
 main = do
-  jsonContent <- B.readFile "data/netflix_titles_nov_2019.json"
-  let maybeShowInfo = decode jsonContent :: Maybe ShowInfo
-  case maybeShowInfo of
-    Just showInfo -> print showInfo
-    Nothing -> putStrLn "No se pudo decodificar el JSON."
+  -- Leemos el contenido del archivo CSV.
+  csvData <- BL.readFile "data/netflix_titles_nov_2019.csv"
+  -- Decodificamos el contenido como un CSV con cabecera.
+  case decodeByName csvData :: Either String (Header, V.Vector ShowInfo) of
+    Left err -> putStrLn err
+    Right (_, v) -> V.forM_ v $ \showInfo ->
+      -- Aquí puedes trabajar con cada 'showInfo' que es una fila del CSV.
+      print showInfo
