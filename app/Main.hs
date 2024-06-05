6c16dcd360532003d522d9bcb7482522e2f67b57
CudrisMartin <09azza.meca@gmail.com>
2024-06-05 18:15:28 -0500

    Comentarios y limpieza del codigo

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,9 @@
+
+-- Programa para analisis de las series y peliculas de Netflix hasta el 2021
+-- Participantes:
+-- Nestor Alejandro Morales Martinez - 20212020065
+-- Martin Elias Cudris Aguilar - 20212020066
+
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -8,7 +14,6 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv hiding ((.=))
 import qualified Data.Vector as V
 import qualified Data.Map.Strict as Map
-import Data.List.Split (splitOn)
 import Data.Char (isDigit)
 import Data.List (sortBy) -- Importamos sortBy de Data.List
 import Data.Ord (comparing) -- Importamos comparing de Data.Ord
@@ -51,7 +56,7 @@ instance FromNamedRecord ShowInfo where
 extractDuration :: String -> Int
 extractDuration s = read $ filter isDigit s
 
--- Función para generar el diagrama circular con los 10 países más frecuentes.
+-- Función para generar el diagrama circular con los 10 países más frecuentes (Despues de una hora de ejecución no soltó nada).
 generatePieChart :: [(String, Int)] -> IO ()
 generatePieChart top10Countries = toFile def "country_pie_chart.svg" $ do
     pie_title .= "Top 10 Países Más Frecuentes"
@@ -76,16 +81,22 @@ main = do
 
       -- Contamos los tipos de shows.
       let typeCounts = V.foldl' (\acc showInfo -> Map.insertWith (+) (type' showInfo) 1 acc) Map.empty v
+
       -- Filtramos y ordenamos las películas por duración.
       let movies = V.filter (\showInfo -> type' showInfo == "Movie") v
-      let sortedMovies = sortBy (comparing (negate . extractDuration . duration)) (V.toList movies)
-      let top10Movies = take 10 sortedMovies
+      let sortedMovies = sortBy (comparing (negate . extractDuration . duration)) (V.toList movies) -- Ordenamos segun las duraciones más altas
+      let top10Movies = take 10 sortedMovies -- Tomamos los primeros 10 resultados
+
       -- Imprimimos el resultado de los 10 países más frecuentes.
       putStrLn "Top 10 países más frecuentes:"
-      let top10Countries = take 10 . sortBy (comparing (negate . snd)) . Map.toList $ countryCounts
+      let top10Countries = take 10 . sortBy (comparing (negate . snd)) . Map.toList $ countryCounts -- Tomamos los primeros 10 resultados
       mapM_ (putStrLn . (\(k, v) -> k ++ ": " ++ show v)) top10Countries
-      putStrLn "\nConteo de tipos:"
+
+      -- Imprimimos el resultado de cuantas peliculas y series hay.
+      putStrLn "\nConteo de tipos:" 
       mapM_ (putStrLn . (\(k, v) -> k ++ ": " ++ show v)) (Map.toList typeCounts)
+
+      -- Imprimimos el resultado de cuantas peliculas y series hay.
       putStrLn "\nTop 10 películas por duración:"
       mapM_ (putStrLn . (\showInfo -> title showInfo ++ " - " ++ duration showInfo)) top10Movies
-      generatePieChart top10Countries
+      
